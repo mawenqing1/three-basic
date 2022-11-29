ba8cbb8a36c05db6abae050af8f99908899d10bf
马文卿 <529837505@qq.com>
2022-11-29 20:22:23 +0800

    add axesHelper

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 //创建场景
 const scene = new THREE.Scene();
@@ -21,4 +22,19 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+
+//创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    // controls.update();
+}
+
+render()
